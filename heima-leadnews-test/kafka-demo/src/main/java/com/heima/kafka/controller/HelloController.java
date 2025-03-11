da219603cfa9c1045e6d77482fbf046657b9f33d
Sun_M <sun_ming148@163.com>
2025-03-11 10:36:13 +0800

    对应p156，kafkaStream的demo跑不通

--- heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/controller/HelloController.java
+++ heima-leadnews-test/kafka-demo/src/main/java/com/heima/kafka/controller/HelloController.java
@@ -2,21 +2,21 @@ package com.heima.kafka.controller;
 
 import com.alibaba.fastjson.JSON;
 import com.heima.kafka.pojo.User;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequiredArgsConstructor
 public class HelloController {
 
-    private final KafkaTemplate<String,String> kafkaTemplate;
+    @Autowired
+    private KafkaTemplate<String,String> kafkaTemplate;
 
     @GetMapping("/hello")
     public String hello(){
 //        kafkaTemplate.send("itcast-topic","黑马程序员");
+
 
         User user = new User();
         user.setUsername("xiaowang");
