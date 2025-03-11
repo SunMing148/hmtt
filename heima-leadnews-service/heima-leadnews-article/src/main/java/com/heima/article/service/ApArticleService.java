99b26b1fc9f8642acd0a313ae230d71a1304c5c0
Sun_M <sun_ming148@163.com>
2025-03-11 16:43:54 +0800

    day11完成，最终测试报错未解决

--- heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
+++ heima-leadnews-service/heima-leadnews-article/src/main/java/com/heima/article/service/ApArticleService.java
@@ -3,12 +3,10 @@ package com.heima.article.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.heima.model.article.dtos.ArticleDto;
 import com.heima.model.article.dtos.ArticleHomeDto;
+import com.heima.model.mess.ArticleVisitStreamMess;
 import com.heima.model.article.pojos.ApArticle;
 import com.heima.model.common.dtos.ResponseResult;
-import org.springframework.web.bind.annotation.RequestBody;
 import com.heima.model.article.dtos.ArticleInfoDto;
-
-import java.io.IOException;
 
 public interface ApArticleService extends IService<ApArticle> {
 
@@ -42,4 +40,11 @@ public interface ApArticleService extends IService<ApArticle> {
      * @return
      */
     public ResponseResult load2(ArticleHomeDto dto,Short type,boolean firstPage);
+
+    /**
+     * 更新文章的分值  同时更新缓存中的热点文章数据
+     * @param mess
+     */
+    public void updateScore(ArticleVisitStreamMess mess);
+
 }
